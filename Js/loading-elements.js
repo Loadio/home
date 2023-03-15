a7a6bbc357be4988dd81dd4077317491dcf95943
Loadio <120505273+Loadio@users.noreply.github.com>
2023-03-15 15:40:38 -0300

    Add files via upload

---
+++ Js/loading-elements.js
@@ -0,0 +1,8 @@
+//Carregar os arquivos para a pagina loadio principal
+
+
+//export elements
+ localStorage.setItem("shortcut-icon", $('link[rel="shortcut icon"]')[0].href)
+ localStorage.setItem("header-ld", $('#loadio-header-element').html()) //Header export
+ localStorage.setItem("footer-ld", $('#Ldhome_loadio-footer-element_62825').html()) //Footer export
+ localStorage.setItem("loading_dots_animation_loadio", $(".Loadio_loading_dots").html())
