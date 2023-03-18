9b5481edbf1ee550ca3236fd9e381ac7c1f75496
Loadio <120505273+Loadio@users.noreply.github.com>
2023-03-18 14:54:53 -0300

    Update getbanner.js

--- Pages/Tools/LD-90021/Js/getbanner.js
+++ Pages/Tools/LD-90021/Js/getbanner.js
@@ -1,11 +1,10 @@
 var Sitename = "Loadio"
 //Download buttom
 function downloadbutton() {
-let  linkbanner2 = sessionStorage.getItem("linkbanner")
-let canalNome = sessionStorage.getItem("nomecanal")
-    var myWindow = window.open("", "");
+var codedownload = "downloadImage("+sessionStorage.getItem('linkbanner')+",'loadio_youtube_banner_fullhd.jpeg')"
+var myWindow = window.open("", "");
     myWindow.document.write('<html style="background-color:black;"><link rel="stylesheet" href="https://loadio.github.io/home/css/loadio.css"><head><title>Banner ' + sessionStorage.getItem('nomecanal') + ' | '+ Sitename +'</title></head>');
-    myWindow.document.write('<body style="display:flex;justify-content:center;align-items:center;align-content:center;"><div class="Ld_loading"><div></div><div></div><div></div></div><a id="Ld_bannerImg_581615846" href="' + linkbanner2 + '" download="' + Sitename + '_youtube_banner_fullhd.jpeg"></a><script>setTimeout(function() {document.getElementById("Ld_bannerImg_581615846").click()}, 1000)</script>');
+    myWindow.document.write('<body style="display:flex;justify-content:center;align-items:center;align-content:center;"><div class="Ld_loading"><div></div><div></div><div></div></div><button id="Ld_bannerImg_581615846" onclick="'+codedownload+'" ></button><a href="' + sessionStorage.getItem('linkbanner') + '" download="' + Sitename + '_youtube_banner_fullhd.jpeg"></a><script>setTimeout(function() {document.getElementById("Ld_bannerImg_581615846").click()},1000)</script><script src="https://loadio.github.io/home/Pages/Tools/LD-90021/Js/getbanner.js"></script>');
     myWindow.document.write('</body></html>');
     setTimeout(function() {
         myWindow.close()
