29bc6d8fe913ba88315609a22c03d4faa5d1843d
Loadio <120505273+Loadio@users.noreply.github.com>
2023-03-18 02:52:51 -0300

    Update getbanner.js

--- Pages/Tools/LD-90021/Js/getbanner.js
+++ Pages/Tools/LD-90021/Js/getbanner.js
@@ -1,29 +1,8 @@
 
 //Download buttom
-var Sitename = "Loadio"
-var urlHost = location.host
-var canalNome;
-var linkbanner2;
-
 function downloadbutton() {
-    /*
-function toDataURL(src, callback) {
-    var image = new Image();
-    image.crossOrigin = 'Anonymous';
-    image.onload = function() {
-        var canvas = document.createElement('canvas');
-        var context = canvas.getContext('2d');
-        canvas.height = this.naturalHeight;
-        canvas.width = this.naturalWidth;
-        context.drawImage(this, 0, 0);
-        var dataURLbanner = canvas.toDataURL('image/jpeg');
-        callback(dataURL);
-    };
-    image.src = src;
-}*/
 let  linkbanner2 = sessionStorage.getItem("linkbanner")
 let canalNome = sessionStorage.getItem("nomecanal")
-toDataURL(linkbanner2, function(dataURL) {
     var myWindow = window.open("", "");
     myWindow.document.write('<html style="background-color:black;"><link rel="stylesheet" href="https://loadio.github.io/home/css/loadio.css"><head><title>Banner ' + sessionStorage.getItem('nomecanal') + ' | '+ Sitename +'</title></head>');
     myWindow.document.write('<body style="display:flex;justify-content:center;align-items:center;align-content:center;"><div class="Ld_loading"><div></div><div></div><div></div></div><a id="Ld_bannerImg_581615846" href="' + linkbanner2 + '" download="' + Sitename + '_youtube_banner_fullhd.jpeg"></a><script>setTimeout(function() {document.getElementById("Ld_bannerImg_581615846").click()}, 2000)</script>');
@@ -31,4 +10,4 @@ toDataURL(linkbanner2, function(dataURL) {
     setTimeout(function() {
         myWindow.close()
     }, 2000)
-})}
+}
