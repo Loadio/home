aa0f5e6b7c243b2b8b67adf34041d0deb469a934
Loadio <120505273+Loadio@users.noreply.github.com>
2023-04-12 19:29:28 -0300

    Add files via upload

---
+++ Js/Allpages-sync/loadioElements_import.js
@@ -0,0 +1,62 @@
+/*------------------------------//
+ Elementos site Header and Footer
+//-------------------------------*/
+
+var header_ld_element = '<header><div class="Ldhome_itens-header_54094"><nav class="navbar"><div class="navbar-brand"><div class="menu_icon"><span class="one"></span><span class="two"></span><span class="three"></span></div><a class="navbar-item"><div class="logo_66831"><div class="logo-img"><a href="/index.html" alt="Logo da Loadio"><svg class="logo-loadio" xmlns="http://www.w3.org/2000/svg" width="841.89" height="271.691" viewBox="0 0 841.89 271.691" style="enable-background:new 0 0 841.89 271.691" xml:space="preserve"><path d="M44.495 50.499h51.978v173.259H44.495V50.499zm68.806 110.143c0-38.859 30.691-66.58 73.759-66.58s73.759 27.721 73.759 66.58c0 38.86-30.691 66.581-73.759 66.581s-73.759-27.721-73.759-66.581zm73.759 22.029c12.375 0 21.286-9.405 21.286-22.523s-8.911-22.524-21.286-22.524-21.286 9.406-21.286 22.524 8.91 22.523 21.286 22.523zm118.559 30.938c-14.603-10.891-27.474-29.459-27.474-55.69 0-35.685 26.979-63.857 70.046-63.857s73.759 27.721 73.759 67.299c0 1.096-.022 2.136-.065 3.121l.065 59.276h-53.216v-8.415c-17.776 9.308-41.701 12.394-63.115-1.734zm42.572-30.938c12.375 0 21.286-9.405 21.286-22.523s-8.911-22.524-21.286-22.524-21.286 9.406-21.286 22.524 8.91 22.523 21.286 22.523zm84.647-23.267c0-36.879 27.227-65.343 62.868-65.343 13.613 0 24.504 4.207 31.435 12.375V50.499h51.978v104.203c0 42.572-29.949 72.521-72.769 72.521-42.326 0-73.512-28.959-73.512-67.819zm73.759 23.762c12.376 0 21.286-9.653 21.286-23.019 0-13.366-8.91-23.02-21.286-23.02s-21.286 9.653-21.286 23.02c0 13.366 8.91 23.019 21.286 23.019zm90.836-85.64h51.978v126.231h-51.978zm68.805 63.116c0-38.859 30.691-66.58 73.759-66.58s73.759 27.721 73.759 66.58c0 38.86-30.691 66.581-73.759 66.581s-73.759-27.721-73.759-66.581zm73.759 22.029c12.376 0 21.286-9.405 21.286-22.523s-8.91-22.524-21.286-22.524-21.286 9.406-21.286 22.524 8.91 22.523 21.286 22.523z"/></svg></a></div></div></a><div class="menu_icon_search"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></div></div><div class="navbar-menu_"><div id="navMenubd-example" class="navbar-menu"><div class="navbar-start"><div class="navbar-item has-dropdown is-hoverable"><a class="navbar-link is-active">Plataforma<div class="navbar-item-alert"><small>hover here</small></div></a><div class="navbar-dropdown"><a class="navbar-item" href="/Privacy-policy.html"><i class="fa-solid fa-shield-halved"></i>Politica de Privacidade</a><a class="navbar-item" href="/terms.html"><i class="fa-solid fa-file-circle-exclamation"></i>Termos e Condiçõess</a><a class="navbar-item" href="/about.html"><i class="fa-solid fa-circle-question"></i>Sobre</a></div></div><div class="navbar-item has-dropdown is-hoverable"><a class="navbar-link is-active">Em criação</a><div class="navbar-dropdown"><a class="navbar-item" href="/web-tools.html"><i class="fa-solid fa-layer-group"></i>Em criação</a><a class="navbar-item" href="/web-tools.html"><i class="fa-solid fa-wand-magic-sparkles"></i>Em criação</a><a class="navbar-item" href="/web-tools.html"><i class="fa-solid fa-triangle-exclamation"></i>Em criação</a></div></div><div class="navbar-item has-dropdown is-hoverable"><a class="navbar-link is-active">Em criação</a><div class="navbar-dropdown"><a class="navbar-item" href="/web-tools.html"><i class="fa-solid fa-fire"></i>Em criação</a><a class="navbar-item" href="/web-tools.html"><i class="fa-solid fa-up-right-from-square"></i>Em criação</a><a class="navbar-item" href="/web-tools.html"><i class="fa-solid fa-screwdriver-wrench"></i>Em criação</a></div></div></div><a class="navbar-item" href="#"><span class="bd-emoji">🎨</span>&nbsp;Em criação</a><a class="navbar-item" href="#"><span class="bd-emoji">❤️</span>&nbsp;Em criação</a></div><div class="navbar-end"><a class="navbar-item is-hidden-desktop-only" href="#" target="_blank"><span class="icon" style="color:#333"><i class="fab fa-github"></i></span></a><a class="navbar-item is-hidden-desktop-only" href="#" target="_blank"><span class="icon" style="color:#55acee"><i class="fab fa-twitter"></i></span></a><div class="navbar-item"><div class="field is-grouped"><div class="menu_icon_search"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg></div><p class="control" onclick="alinkclick(2)"><a class="button is-primary" target="_blank" href="https://apoia.se/loadio"><span class="icon"><i class="fas fa-donate"></i></span><span>Ajudar o site</span></a></p></div></div></div></div></nav></div></header>'
+
+var footer_ld_element = '<footer><div id="content-footer_88645"><div class="logo-footer_56497"><a href="#"><svg class="logo-loadio" xmlns="http://www.w3.org/2000/svg" width="841.89" height="271.691" viewBox="0 0 841.89 271.691" style="enable-background:new 0 0 841.89 271.691" xml:space="preserve"><path d="M44.495 50.499h51.978v173.259H44.495V50.499zm68.806 110.143c0-38.859 30.691-66.58 73.759-66.58s73.759 27.721 73.759 66.58c0 38.86-30.691 66.581-73.759 66.581s-73.759-27.721-73.759-66.581zm73.759 22.029c12.375 0 21.286-9.405 21.286-22.523s-8.911-22.524-21.286-22.524-21.286 9.406-21.286 22.524 8.91 22.523 21.286 22.523zm118.559 30.938c-14.603-10.891-27.474-29.459-27.474-55.69 0-35.685 26.979-63.857 70.046-63.857s73.759 27.721 73.759 67.299c0 1.096-.022 2.136-.065 3.121l.065 59.276h-53.216v-8.415c-17.776 9.308-41.701 12.394-63.115-1.734zm42.572-30.938c12.375 0 21.286-9.405 21.286-22.523s-8.911-22.524-21.286-22.524-21.286 9.406-21.286 22.524 8.91 22.523 21.286 22.523zm84.647-23.267c0-36.879 27.227-65.343 62.868-65.343 13.613 0 24.504 4.207 31.435 12.375V50.499h51.978v104.203c0 42.572-29.949 72.521-72.769 72.521-42.326 0-73.512-28.959-73.512-67.819zm73.759 23.762c12.376 0 21.286-9.653 21.286-23.019 0-13.366-8.91-23.02-21.286-23.02s-21.286 9.653-21.286 23.02c0 13.366 8.91 23.019 21.286 23.019zm90.836-85.64h51.978v126.231h-51.978zm68.805 63.116c0-38.859 30.691-66.58 73.759-66.58s73.759 27.721 73.759 66.58c0 38.86-30.691 66.581-73.759 66.581s-73.759-27.721-73.759-66.581zm73.759 22.029c12.376 0 21.286-9.405 21.286-22.523s-8.91-22.524-21.286-22.524-21.286 9.406-21.286 22.524 8.91 22.523 21.286 22.523z"></path></svg></a></div><div class="element-separator"><span class="divider-separator"></span></div><div class="credit"><p style="color:rgba(255,255,255,.325)">Criado por Atomic Tuner - © 2023 Loadio.</p></div></div></footer>'
+
+
+
+
+
+/*------------------------------//
+Importar Elementos da pagina da loadio principal
+//-------------------------------*/
+
+//Nome ferramenta e do site para nome projeto
+var tool_name = location.pathname.split(/(\\|\/)/g).pop().split('.html')
+document.title = tool_name[0].replaceAll('-',' ') + " | Loadio";
+$("head title").text(function(i, text) { //Colocar primeira letra como maiuscula do title head
+  return text.substr(0,1).toUpperCase() + text.substr(1);
+});
+
+  //Sem suporte resolução
+if ($(window).width() < 310) {
+  document.body.innerHTML = '<style>body{background:white;}.Ld_loading div{background-color:black;}}</style><div style="width: 100%;height: 100%;display: flex;justify-content: center;align-items: center;position: absolute;flex-direction: column;"><p style="font-weight:600;padding: 10px;text-align: center;">A Loadio não suporta esta resolução, por favor aumente a resolução do seu aparelho, e recarrege a página!!</p><div class="Ld_loading"><div></div><div></div><div></div></div></div>';
+}
+
+//Import elements
+var shortcut_icon_export = localStorage.getItem('shortcut-icon')
+$('<link rel="shortcut icon" href="'+shortcut_icon_export+'" type="image/x-icon">').appendTo("head");
+var header_export = localStorage.getItem('header-ld')  || header_ld_element
+$("#loadio-header-element-export").html(header_export); //Import header
+var footer_export = localStorage.getItem('footer-ld') || footer_ld_element
+$("#loadio-footer-element-export").html(footer_export); //Import footer
+var loading_dots_animation = localStorage.getItem('loading_dots_animation_loadio')
+$('#preloader .loader').html(loading_dots_animation)
+
+//Verifica se tem um erro nas 'etp' e Criar elemento (Error cunstom)
+function checkept(){
+    setTimeout( function (){
+    let etp1 = sessionStorage.getItem('etp1');
+    let etp2 = sessionStorage.getItem('etp2');
+    if (etp1 == etp2) { //Checagem das etp 1,2 derão true(Funcionanram)
+      putnamechannel();
+      $('.Error-cusntom').remove();
+      sessionStorage.removeItem('etp1')
+      sessionStorage.removeItem('etp2')
+    } else { //Checagem das etp 1,2 derão false(Não funcionanram)
+      let loadio_error_images_found = "/Img/error/error-found-laodio01.png"
+$('<div class="Error-cusntom"><div class="Error-custom-element"><h2>Nada foi encontrado!</h2><p>Este canal não existe, ou não foi encontrado, por favor verifique se o link do canal está correto!</p></div></div>').appendTo("#content")
+$('#content').css('transition', 'all 0.5s ease-out 0s');
+$('#content').css('background-color', 'var(--global-pallet-color-blue-error-bg)');
+$('#content').css('display', 'flex');
+$('#content').css('height', '0px');
+$('#content').css('height', '460px');
+$('#preloader').css('display', 'none');
+      console.log("Deu error!!!")
+      sessionStorage.removeItem('etp1')
+      sessionStorage.removeItem('etp2')
+    }},5500)
+    }
