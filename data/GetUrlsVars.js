2dca6e6cd810752addec3cfe8bf75607c2810a16
Loadio <120505273+Loadio@users.noreply.github.com>
2023-04-12 19:35:41 -0300

    Add files via upload

---
+++ data/GetUrlsVars.js
@@ -0,0 +1,10 @@
+//------------------------
+  //Found on https://stackoverflow.com/a/20097994/5394675
+  function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+    function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+  }
---
+++ data/posts_tools.js
@@ -0,0 +1,365 @@
+// As de cima são as mais recentes
+var LDtoolsPosts = 
+[
+{
+    "titulo":"Coletor de banner youtube",
+    "tags":{
+        "tag1":"Youtube", 
+        "tag2":" | ",
+        "tag3":"Miniatura ",
+        "tag4":" | ", 
+        "tag5":"Imagens ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://www.wyzowl.com/wp-content/uploads/2019/09/The-11-best-YouTube-banner-makers.-Make-epic-channel-art.png",
+    },
+    "descrição":"Colete banner de canais do Youtube e use onde você quiser!Com essa ferramenta será possível baixar banners de canais do Yotube,já que o Youtube não dá essa opção para você. Também, você poderá baixar banner der outros canais, não só o seu :3",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"https://youtube.com",
+    "selecttool_Id":"LD-90021"
+},
+{
+    "titulo":"Ferrrmanta 2 em desenvolvimento",
+    "tags":{
+        "tag1":"Youtube", 
+        "tag2":" | ",
+        "tag3":"Miniatura ",
+        "tag4":" | ", 
+        "tag5":"Imagens ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/seed/picsum/500/500?random=2"
+    },
+    "descrição":"2 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"https://picsum.photos/seed/picsum/500/500?random=2",
+    "selecttool_Id":"LD-90121"
+},
+{
+    "titulo":"Ferrrmanta 3 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/237/500/500"
+    },
+    "descrição":"3 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"https://picsum.photos/id/237/500/500",
+    "selecttool_Id":"LD-50021"
+},
+{
+    "titulo":"Ferrrmanta 4 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/27/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+}
+,
+{
+    "titulo":"Ferrrmanta 5 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/28/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 6 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/29/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 7 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/30/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 8 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/37/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 9 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/327/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 10 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/34/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 11 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/377/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 12 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/384/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 13 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/304/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 14 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/305/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 15 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/315/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 16 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/306/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 17 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/307/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 18 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/308/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 19 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/309/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+},
+{
+    "titulo":"Ferrrmanta 20 em desenvolvimento",
+    "tags":{
+        "tag1":"Video", 
+        "tag2":" | ",
+        "tag3":"Rede Social",
+        "tag4":" | ", 
+        "tag5":"Download ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://picsum.photos/id/320/500/500"
+    },
+    "descrição":"4 Colete banner de canais do youtube, e baixe para você usar em outro lugar.",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-40051"
+}
+]
