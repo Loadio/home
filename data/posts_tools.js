508b03d875152fa61cac0f6764ab753dd41edac2
Loadio <120505273+Loadio@users.noreply.github.com>
2023-04-13 08:50:52 -0300

    Update posts_tools.js

--- data/posts_tools.js
+++ data/posts_tools.js
@@ -2,40 +2,21 @@
 var LDtoolsPosts = 
 [
 {
-    "titulo":"Coletor de banner youtube",
+    "titulo":"Testador de Thumbnail",
     "tags":{
         "tag1":"Youtube", 
         "tag2":" | ",
-        "tag3":"Banner ",
+        "tag3":"Miniatura ",
         "tag4":" | ", 
         "tag5":"Imagens ",
     },
     "urlImage":
     {
-        "imagem_ferramenta":"https://www.wyzowl.com/wp-content/uploads/2019/09/The-11-best-YouTube-banner-makers.-Make-epic-channel-art.png",
+        "imagem_ferramenta":"",
     },
-    "descrição":"Colete banner de canais do Youtube e use onde você quiser!Com essa ferramenta será possível baixar banners de canais do Yotube,já que o Youtube não dá essa opção para você. Também, você poderá baixar banner der outros canais, não só o seu :3",
-    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "descrição":"Em criação no momento",
     "link_ferramenta_externa":"",
-    "selecttool_Id":"LD-90021"
-},
-{
-    "titulo":"Coletor de banner youtube",
-    "tags":{
-        "tag1":"Youtube", 
-        "tag2":" | ",
-        "tag3":"Banner ",
-        "tag4":" | ", 
-        "tag5":"Imagens ",
-    },
-    "urlImage":
-    {
-        "imagem_ferramenta":"https://www.wyzowl.com/wp-content/uploads/2019/09/The-11-best-YouTube-banner-makers.-Make-epic-channel-art.png",
-    },
-    "descrição":"Colete banner de canais do Youtube e use onde você quiser!Com essa ferramenta será possível baixar banners de canais do Yotube,já que o Youtube não dá essa opção para você. Também, você poderá baixar banner der outros canais, não só o seu :3",
-    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
-    "link_ferramenta_externa":"",
-    "selecttool_Id":"LD-90021"
+    "selecttool_Id":"LD-90215"
 },
 {
     "titulo":"Coletor de banner youtube",
