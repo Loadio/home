6cb9eba8e2ea7b35c37770aa6923f9aecced3e68
Loadio <120505273+Loadio@users.noreply.github.com>
2023-04-13 08:33:56 -0300

    Update posts_tools.js

--- data/posts_tools.js
+++ data/posts_tools.js
@@ -2,22 +2,40 @@
 var LDtoolsPosts = 
 [
 {
-    "titulo":"Testador de thumbnail",
+    "titulo":"Coletor de banner youtube",
     "tags":{
         "tag1":"Youtube", 
         "tag2":" | ",
-        "tag3":"Miniatura ",
+        "tag3":"Banner ",
         "tag4":" | ", 
         "tag5":"Imagens ",
     },
     "urlImage":
     {
-        "imagem_ferramenta":"https://loadio.github.io/home/Img/error/No%20image_loadio.jpg",
+        "imagem_ferramenta":"https://www.wyzowl.com/wp-content/uploads/2019/09/The-11-best-YouTube-banner-makers.-Make-epic-channel-art.png",
     },
-    "descrição":"Em criação no momento...",
-    "link_ferramenta":"",
+    "descrição":"Colete banner de canais do Youtube e use onde você quiser!Com essa ferramenta será possível baixar banners de canais do Yotube,já que o Youtube não dá essa opção para você. Também, você poderá baixar banner der outros canais, não só o seu :3",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
     "link_ferramenta_externa":"",
-    "selecttool_Id":"LD-90200"
+    "selecttool_Id":"LD-90021"
+},
+{
+    "titulo":"Coletor de banner youtube",
+    "tags":{
+        "tag1":"Youtube", 
+        "tag2":" | ",
+        "tag3":"Banner ",
+        "tag4":" | ", 
+        "tag5":"Imagens ",
+    },
+    "urlImage":
+    {
+        "imagem_ferramenta":"https://www.wyzowl.com/wp-content/uploads/2019/09/The-11-best-YouTube-banner-makers.-Make-epic-channel-art.png",
+    },
+    "descrição":"Colete banner de canais do Youtube e use onde você quiser!Com essa ferramenta será possível baixar banners de canais do Yotube,já que o Youtube não dá essa opção para você. Também, você poderá baixar banner der outros canais, não só o seu :3",
+    "link_ferramenta":"Tools/LD-90021/Pick-banner-youtube.html",
+    "link_ferramenta_externa":"",
+    "selecttool_Id":"LD-90021"
 },
 {
     "titulo":"Coletor de banner youtube",
