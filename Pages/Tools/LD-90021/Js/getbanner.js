75228f410672724c3e7133d816f1002ee57a19da
Loadio <120505273+Loadio@users.noreply.github.com>
2023-07-07 00:51:48 -0300

    Update getbanner.js

--- Pages/Tools/LD-90021/Js/getbanner.js
+++ Pages/Tools/LD-90021/Js/getbanner.js
@@ -12,7 +12,7 @@ var myWindow = window.open("", "");
     myWindow.document.write('</body></html>');
     setTimeout(function() {
         myWindow.close()
-    }, 500)
+    }, 1500)
 }
 function downloadImage(url, name){
       fetch(url)
